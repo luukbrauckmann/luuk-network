cac24dad07cf30635b2d2e0a6739d53f8663c8a7
Luuk <luukbrauckmann@gmail.com>
2024-01-25 16:44:00 +0100

    Commit

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,6 +5,18 @@ export default defineNuxtConfig({
       title: "Luuk Brauckmann",
       charset: "utf-8",
       viewport: "width=device-width, initial-scale=1",
+      meta: [
+        {
+          name: 'color-scheme',
+          content: 'light dark'
+        }
+      ]
+    }
+  },
+  runtimeConfig: {
+    public: {
+      datocmsApiToken: process.env.DATOCMS_API_TOKEN,
+      datocmsGraphqlUrl: process.env.DATOCMS_GRAPHQL_URL,
     },
-  }
+  },
 })
