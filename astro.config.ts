3d639afc7a96eb62371dd59cfd0f1f0adc18178e
Luuk <luukbrauckmann@gmail.com>
2024-02-13 10:06:58 +0100

    Dynamic robots.txt

--- astro.config.ts
+++ astro.config.ts
@@ -3,10 +3,14 @@ import { defineConfig } from "astro/config";
 import sitemap from '@astrojs/sitemap';
 import type { PluginOption } from 'vite';
 
+const port = 4321;
+export const site = process.env.CF_PAGES ? process.env.CF_PAGES_URL : `http://localhost:${port}`;
+
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://luukbrauckmann.dev',
+  site,
   integrations: [sitemap()],
+  server: { port },
   vite: {
     plugins: [
       graphql() as PluginOption,
---
+++ src/pages/robots.txt.ts
@@ -0,0 +1,18 @@
+import type { APIRoute } from 'astro';
+
+const robotsTxt = ({ site }: { site: string }) => `
+User-agent: *
+Allow: /
+
+Sitemap: ${site}/sitemap-index.xml
+`.trim();
+
+export const GET: APIRoute = (context) => {
+  return new Response(robotsTxt({
+    site: context.site!.origin,
+  }), {
+    headers: {
+      'content-type': 'text/plain',
+    },
+  });
+};
