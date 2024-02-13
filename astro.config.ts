5215428638fa0a72bc6fa72afce06f160ba2c02c
Luuk <luukbrauckmann@gmail.com>
2024-02-13 09:55:37 +0100

    Added sitemap

--- astro.config.ts
+++ astro.config.ts
@@ -1,9 +1,11 @@
 import graphql from "@rollup/plugin-graphql";
 import { defineConfig } from "astro/config";
+import sitemap from '@astrojs/sitemap';
 import type { PluginOption } from 'vite';
 
 // https://astro.build/config
 export default defineConfig({
+  integrations: [sitemap()],
   vite: {
     plugins: [
       graphql() as PluginOption,
