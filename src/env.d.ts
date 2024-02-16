7a9e7ea0a16a0c57d1463c25a5e1e9c93ea95e69
Luuk <luukbrauckmann@gmail.com>
2024-02-16 15:04:23 +0100

    graphql decl removed

--- src/env.d.ts
+++ src/env.d.ts
@@ -3,9 +3,3 @@
 interface ImportMetaEnv {
   readonly DATOCMS_API_KEY: string;
 }
-
-declare module "*.query.graphql" {
-  import { DocumentNode } from "graphql";
-  const value: DocumentNode;
-  export = value;
-}
