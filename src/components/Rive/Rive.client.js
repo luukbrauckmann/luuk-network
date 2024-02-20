0864b1f422625b8324f9b1c184e4e7486a1d94d8
Luuk <luukbrauckmann@gmail.com>
2024-02-20 16:43:09 +0100

    Pref test

--- src/components/Rive/Rive.client.js
+++ src/components/Rive/Rive.client.js
@@ -1,17 +1,16 @@
-import riveWasmUrl from '@rive-app/canvas/rive.wasm?url';
+import riveWASMResource from '@rive-app/canvas/rive.wasm?url';
 import { RuntimeLoader, Alignment, Fit, Layout, Rive } from "@rive-app/canvas";
 
-RuntimeLoader.setWasmUrl(riveWasmUrl);
+RuntimeLoader.setWasmUrl(riveWASMResource)
 
 class RiveCanvas extends HTMLElement {
   constructor() {
     super();
 
     const canvas = document.querySelector("canvas");
-    const alt = document.querySelector("p");
 
     const { src, fit, alignment, autoplay, statemachines } = canvas.dataset;
-
+    
     const layout = new Layout({ fit: Fit[fit], alignment: Alignment[alignment] });
 
     const rive = new Rive({ 
