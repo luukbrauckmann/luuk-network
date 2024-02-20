a8b3d15c998ff249d29cca1239efc02c96957eba
Luuk <luukbrauckmann@gmail.com>
2024-02-20 11:22:18 +0100

    Removed console logs

--- src/components/Rive/Rive.client.js
+++ src/components/Rive/Rive.client.js
@@ -7,8 +7,6 @@ class RiveCanvas extends HTMLElement {
     const canvas = document.querySelector("canvas");
     const { src, fit, alignment, autoplay, statemachines } = canvas.dataset;
 
-    console.log(canvas.dataset);
-
     const layout = new Layout({ fit: Fit[fit], alignment: Alignment[alignment] });
 
     const rive = new Rive({ 
