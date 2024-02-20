4b61e2a2f64b9dab8c40c9133cef822cbcaa7f7f
Luuk <luukbrauckmann@gmail.com>
2024-02-20 18:19:45 +0100

    Add partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,38 +1,33 @@
 import graphql from "@rollup/plugin-graphql";
 import topLevelAwait from "vite-plugin-top-level-await";
 import wasm from "vite-plugin-wasm";
-
 import { defineConfig, passthroughImageService } from "astro/config";
-
 import cloudflare from "@astrojs/cloudflare";
 import sitemap from "@astrojs/sitemap";
-
+import partytown from "@astrojs/partytown";
 const port = 4321;
-export const site = process.env.CF_PAGES
-  ? process.env.CF_PAGES_URL
-  : `http://localhost:${port}`;
-  
+export const site = process.env.CF_PAGES ? process.env.CF_PAGES_URL : `http://localhost:${port}`;
+
+// https://astro.build/config
 export default defineConfig({
   adapter: cloudflare({
     mode: "directory",
     functionPerRoute: true,
     runtime: {
       mode: "local",
-      type: "pages",
-    },
+      type: "pages"
+    }
   }),
   image: {
-    service: passthroughImageService(),
+    service: passthroughImageService()
   },
   output: "hybrid",
   site,
-  integrations: [sitemap()],
-  server: { port },
-  vite: {
-    plugins: [
-      graphql(),
-      topLevelAwait(),
-      wasm()
-    ],
+  integrations: [sitemap(), partytown()],
+  server: {
+    port
   },
+  vite: {
+    plugins: [graphql(), topLevelAwait(), wasm()]
+  }
 });
