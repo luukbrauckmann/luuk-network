660536583a2b01a887371e74fa474c374025b83b
Luuk <luukbrauckmann@gmail.com>
2024-02-21 15:42:01 +0100

    Rive back to main thread

--- src/components/Rive/Rive.client.js
+++ src/components/Rive/Rive.client.js
@@ -1,14 +1,28 @@
-import RiveWorker from "./Rive.worker.js?worker";
+import { Alignment, Fit, Layout, Rive, RuntimeLoader } from "@rive-app/canvas";
+import riveWASMResource from "@rive-app/canvas/rive.wasm?url";
+
+RuntimeLoader.setWasmUrl(riveWASMResource);
 
 class RiveCanvas extends HTMLElement {
   constructor() {
     super();
 
     const canvas = this.querySelector("canvas");
-    const offscreen = canvas.transferControlToOffscreen();
 
-    const worker = new RiveWorker();
-    worker.postMessage({ canvas: offscreen, ...canvas.dataset }, [offscreen]);
+    const { src, fit, alignment, autoplay, statemachines } = canvas.dataset;
+
+    const layout = new Layout({
+      fit: Fit[fit],
+      alignment: Alignment[alignment],
+    });
+
+    const rive = new Rive({
+      canvas,
+      src,
+      layout,
+      autoplay: typeof autoplay != "undefined",
+      stateMachines: statemachines,
+    });
   }
 }
 
