2ddb9694f8a502bf1c263396d2d6cb796977b837
Luuk <luukbrauckmann@gmail.com>
2024-02-21 11:42:04 +0100

    Add es format for worker

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,16 @@
 import graphql from "@rollup/plugin-graphql";
 import topLevelAwait from "vite-plugin-top-level-await";
 import wasm from "vite-plugin-wasm";
+
 import { defineConfig, passthroughImageService } from "astro/config";
+
 import cloudflare from "@astrojs/cloudflare";
 import sitemap from "@astrojs/sitemap";
 
 const port = 4321;
-export const site = process.env.CF_PAGES ? process.env.CF_PAGES_URL : `http://localhost:${port}`;
+export const site = process.env.CF_PAGES
+  ? process.env.CF_PAGES_URL
+  : `http://localhost:${port}`;
 
 export default defineConfig({
   adapter: cloudflare({
@@ -14,19 +18,22 @@ export default defineConfig({
     functionPerRoute: true,
     runtime: {
       mode: "local",
-      type: "pages"
-    }
+      type: "pages",
+    },
   }),
   image: {
-    service: passthroughImageService()
+    service: passthroughImageService(),
   },
   output: "hybrid",
   site,
   integrations: [sitemap()],
   server: {
-    port
+    port,
   },
   vite: {
-    plugins: [graphql(), topLevelAwait(), wasm()]
-  }
+    plugins: [graphql(), topLevelAwait(), wasm()],
+    worker: {
+      format: "es",
+    },
+  },
 });
