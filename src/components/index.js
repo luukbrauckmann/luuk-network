2294ba88817e599b7fdfb49aa3d17075c3cb0d25
Luuk <luukbrauckmann@gmail.com>
2024-03-07 17:19:16 +0100

    Fixed stupid icon component Added alot of fun stuff

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,6 @@
+export { default as CopyLeft } from './CopyLeft';
+export { default as Icon } from './Icon';
+export { default as IconSprite } from './IconSprite';
 export { default as LocaleSelector } from './LocaleSelector';
 export { default as Nav } from './Nav';
 export { default as ThemeSelector } from './ThemeSelector';
