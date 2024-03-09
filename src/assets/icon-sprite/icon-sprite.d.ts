94a904b6016764d6b4bef57a9b658da79a855d90
Luuk <luukbrauckmann@gmail.com>
2024-03-09 13:00:04 +0100

    Add logo & icons

---
+++ src/assets/icon-sprite/icon-sprite.d.ts
@@ -0,0 +1,1 @@
+export type Name = 'codepen' | 'desktop' | 'discord' | 'en' | 'github' | 'home' | 'instagram' | 'linkedin' | 'logo' | 'magnifying-glass' | 'mastodon' | 'moon' | 'newspaper' | 'nl' | 'person-secret' | 'sun' | 'x';
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,3 @@
 export { default as CopyLeft } from "./CopyLeft/CopyLeft.astro";
+export { default as Icon } from "./Icon/Icon.astro";
+export { default as IconSprite } from "./IconSprite/IconSprite.astro";
