839d6aa1bebba0de0caf9ccc5edb7a645d7a8891
Luuk <luukbrauckmann@gmail.com>
2024-03-12 09:48:36 +0100

    Convert to cloudflare

--- astro.config.ts
+++ astro.config.ts
@@ -1,43 +1,37 @@
-import { defineConfig } from 'astro/config';
-
-import netlify from "@astrojs/netlify";
+import { defineConfig, passthroughImageService } from 'astro/config';
 import graphql from "@rollup/plugin-graphql";
 import alias from "@rollup/plugin-alias";
 import { resolve } from "path";
+import cloudflare from "@astrojs/cloudflare";
 
+// https://astro.build/config
 export default defineConfig({
+  output: "hybrid",
+  adapter: cloudflare(),
   server: {
     port: 4344
   },
-  output: "server",
-  adapter: netlify(),
+  image: {
+    service: passthroughImageService(),
+  },
   vite: {
-    plugins: [
-      graphql(),
-      alias({
-        entries: [
-          {
-            find: "@assets",
-            replacement: resolve("./src/", "assets"),
-          },
-          {
-            find: "@blocks",
-            replacement: resolve("./src/", "blocks"),
-          },
-          {
-            find: "@components",
-            replacement: resolve("./src/", "components"),
-          },
-          {
-            find: "@layouts",
-            replacement: resolve("./src/", "layouts"),
-          },
-          {
-            find: "@lib",
-            replacement: resolve("./src/", "lib"),
-          },
-        ],
-      }),
-    ]
-  }
+    plugins: [graphql(), alias({
+      entries: [{
+        find: "@assets",
+        replacement: resolve("./src/", "assets")
+      }, {
+        find: "@blocks",
+        replacement: resolve("./src/", "blocks")
+      }, {
+        find: "@components",
+        replacement: resolve("./src/", "components")
+      }, {
+        find: "@layouts",
+        replacement: resolve("./src/", "layouts")
+      }, {
+        find: "@lib",
+        replacement: resolve("./src/", "lib")
+      }]
+    })]
+  },
 });
