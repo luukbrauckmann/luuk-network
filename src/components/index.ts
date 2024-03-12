431d3dbadeee2e1093e4ab8b3635ffe0878d18fe
Luuk <luukbrauckmann@gmail.com>
2024-03-12 21:41:43 +0100

    Format and localeselector

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export { default as CopyLeft } from "./CopyLeft/CopyLeft.astro";
 export { default as Icon } from "./Icon/Icon.astro";
 export { default as IconSprite } from "./IconSprite/IconSprite.astro";
+export { default as LocaleSelector } from "./LocaleSelector/LocaleSelector.astro";
