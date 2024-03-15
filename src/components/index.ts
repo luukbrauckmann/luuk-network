12e84362df54b77a7155e5c0cddcfed7210b31fa
Luuk <luukbrauckmann@gmail.com>
2024-03-15 13:24:45 +0100

    Removed Rive again

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,4 +2,3 @@ export { default as CopyLeft } from "./CopyLeft/CopyLeft.astro";
 export { default as Icon } from "./Icon/Icon.astro";
 export { default as IconSprite } from "./IconSprite/IconSprite.astro";
 export { default as LocaleSelector } from "./LocaleSelector/LocaleSelector.astro";
-export { default as Rive } from "./Rive/Rive.astro";
