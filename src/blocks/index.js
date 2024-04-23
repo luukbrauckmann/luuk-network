d0cb7de507253e79af76478bd4e718d6fdb8f9fe
Luuk <me@luuk.network>
2024-04-23 23:14:07 +0200

    Titleblock added

--- src/blocks/index.js
+++ src/blocks/index.js
@@ -1,3 +1,4 @@
 export { default as Blocks } from "./Blocks/Blocks.astro";
 export { default as Hero } from "./Hero/Hero.astro";
 export { default as NotFound } from "./NotFound/NotFound.astro";
+export { default as Title } from "./Title/Title.astro";
