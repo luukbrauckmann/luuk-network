dc51e4af6cb0ea232505d87876389f7a1e847a9a
Luuk <me@luuk.network>
2024-04-24 19:20:02 +0200

    Added fonts and seo stuff

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+export { default as Footer } from "./Footer/Footer.astro";
+export { default as Header } from "./Header/Header.astro";
+export { default as Preload } from "./Preload/Preload.astro";
+export { default as Seo } from "./Seo/Seo.astro";
+export { default as StructuredText } from "./StructuredText/StructuredText.astro";
+export { default as Tag } from "./Tag/Tag.astro";
