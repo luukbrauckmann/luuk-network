4b84927135fe633d02979e43dad432efdbbfaab8
Luuk <me@luuk.network>
2024-05-02 16:30:18 +0200

    Fixed some stuff

--- astro.config.js
+++ astro.config.js
@@ -1,11 +1,15 @@
-import { defineConfig, passthroughImageService } from "astro/config";
+import "dotenv/config";
 
+import { defineConfig, passthroughImageService } from "astro/config";
 import cloudflare from "@astrojs/cloudflare";
 import graphql from "@rollup/plugin-graphql";
+import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
+  site: process.env.CF_PAGES_URL,
   output: "hybrid",
   adapter: cloudflare(),
+  integrations: [sitemap()],
   vite: {
     plugins: [graphql()],
   },
--- src/blocks/index.js
+++ src/blocks/index.js
@@ -1,6 +1,5 @@
 export { default as Blocks } from "./Blocks/Blocks.astro";
 export { default as Hero } from "./Hero/Hero.astro";
-export { default as NotFound } from "./NotFound/NotFound.astro";
 export { default as Section } from "./Section/Section.astro";
 export { default as Text } from "./Text/Text.astro";
 export { default as Title } from "./Title/Title.astro";
